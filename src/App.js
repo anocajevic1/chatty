61f602040c7a77e447a78c73b286ee13fd888ffa
Amna <anocajevic1@etf.unsa.ba>
2020-10-28 21:05:41 +0100

    Database is set up and some basic structure.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAH0NFY6yCU5JNKQ93RTpJ3xAPKbliNkqs",
+  authDomain: "chatty-ace.firebaseapp.com",
+  databaseURL: "https://chatty-ace.firebaseio.com",
+  projectId: "chatty-ace",
+  storageBucket: "chatty-ace.appspot.com",
+  messagingSenderId: "978834290380",
+  appId: "1:978834290380:web:aee10ff3c91be995d77469",
+  measurementId: "G-WN9CWH688E"
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
+}
+
+function SignIn() {
+
+}
+
+function ChatRoom() {
+  
 }
 
 export default App;
