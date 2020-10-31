76f2eaa0a35a5bff0d6ab7ac5eed489bb409b6f1
Amna <anocajevic1@etf.unsa.ba>
2020-10-31 16:25:18 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,  { useRef, useState } from 'react';
 import './App.css';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
@@ -6,16 +6,19 @@ import 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-firebase.initializeApp({
-  apiKey: "AIzaSyAH0NFY6yCU5JNKQ93RTpJ3xAPKbliNkqs",
-  authDomain: "chatty-ace.firebaseapp.com",
-  databaseURL: "https://chatty-ace.firebaseio.com",
-  projectId: "chatty-ace",
-  storageBucket: "chatty-ace.appspot.com",
-  messagingSenderId: "978834290380",
-  appId: "1:978834290380:web:aee10ff3c91be995d77469",
-  measurementId: "G-WN9CWH688E"
-});
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyAH0NFY6yCU5JNKQ93RTpJ3xAPKbliNkqs",
+    authDomain: "chatty-ace.firebaseapp.com",
+    databaseURL: "https://chatty-ace.firebaseio.com",
+    projectId: "chatty-ace",
+    storageBucket: "chatty-ace.appspot.com",
+    messagingSenderId: "978834290380",
+    appId: "1:978834290380:web:aee10ff3c91be995d77469",
+    measurementId: "G-WN9CWH688E"
+  });
+}
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
@@ -26,7 +29,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
       
       </header>
       <section>
@@ -37,11 +40,80 @@ function App() {
 }
 
 function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
 
+  return (<button onClick={signInWithGoogle}>Sign In With Google</button>);
 }
+
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
 
 function ChatRoom() {
-  
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (<>
+    <main>
+
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>🕊️</button>
+
+    </form>
+  </>)
 }
+
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <p>{text}</p>
+    </div>
+  </>)
+}
+
 
 export default App;
